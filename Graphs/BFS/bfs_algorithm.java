339a123227d0c4ffe7932219b023143d90cc3279
prasannabollineni17-ctrl <prasannabollineni17@gmail.com>
2026-09-06 11:21:35 +0530

    Add BFS algorithm implementation in Java
    Implemented BFS algorithm using an adjacency list.

--- Graphs/BFS/bfs_algorithm.java
+++ Graphs/BFS/bfs_algorithm.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // BFS traversal of a graph(represented  using adjacency list)
+        ArrayList<Integer> list= new ArrayList<>();
+        Queue<Integer> q= new LinkedList<>();
+        boolean []  visited= new  boolean[adj.size()];
+        Arrays.fill(visited,false);
+        visited[0]=true;
+        q.add(0);
+        while(!q.isEmpty()){
+            int val=q.poll();
+            list.add(val);
+            for(int num:adj.get(val)){
+                if(!visited[num]){
+                    visited[num]=true;
+                    q.add(num);
+                }
+            }
+        }
+        return list;
+    }
+}
